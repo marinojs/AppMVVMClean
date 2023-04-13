9b6c3757cc5dcbb644571d64f1971407befbe150
Marino Salazar <marino.salazar@globant.com>
2023-04-13 17:14:28 -0400

    added some constants

--- app/src/main/java/com/example/appmvvm/data/remote/model/Constants.kt
+++ app/src/main/java/com/example/appmvvm/data/remote/model/Constants.kt
@@ -1,5 +1,28 @@
 package com.example.appmvvm.data.remote.model
 
 object Constants {
-    const val SERIALIZED_NAME = "name"
+
+    const val SERIALIZED_CREATED_BY = "created_by"
+    const val SERIALIZED_DESCRIPTION = "description"
+    const val SERIALIZED_FAVORITE_COUNT = "favorite_count"
+    const val SERIALIZED_ITEMS = "items"
+
+
+    //Movie
+    const val SERIALIZED_ADULT = "adult"
+    const val SERIALIZED_BACKDROP_PATH = "backdrop_path"
+    const val SERIALIZED_GENRES_ID = "genre_ids"
+    const val SERIALIZED_ID = "id"
+    const val SERIALIZED_MEDIA_TYPE = "media_type"
+    const val SERIALIZED_ORIGINAL_LANGUAGE = "original_language"
+    const val SERIALIZED_ORIGINAL_TITLE = "original_title"
+    const val SERIALIZED_OVERVIEW = "overview"
+    const val SERIALIZED_POPULARITY = "popularity"
+    const val SERIALIZED_POSTER_PATH = "poster_path"
+    const val SERIALIZED_RELEASE_DATE = "release_date"
+    const val SERIALIZED_TITLE = "title"
+    const val SERIALIZED_VIDEO = "video"
+    const val SERIALIZED_VOTE_AVERAGE = "vote_average"
+    const val SERIALIZED_VOTE_COUNT = "vote_count"
+
 }
